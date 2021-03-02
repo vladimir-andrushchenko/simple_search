289b1250409e6923090fcf841df30bd66acbd135
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-03-02 21:42:09 +0300

    Initial Commit

---
+++ simple_search/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  simple_search
+//
+//  Created by Vladimir Andrushchenko on 02.03.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
