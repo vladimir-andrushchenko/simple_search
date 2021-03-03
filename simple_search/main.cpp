3902cb294b8219a84fa5ae155fff6e8a7e4bf33a
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-03-03 20:01:55 +0300

    this version passed test

--- simple_search/main.cpp
+++ simple_search/main.cpp
@@ -65,6 +65,17 @@ void AddDocument(map<string, set<int>>& word_to_documents,
     }
 }
 
+bool ComparePairBySecondThenFirst(pair<int, int> a, pair<int, int> b) {
+    if (a.second > b.second) {
+        return true;
+    } else if (a.second == b.second) {
+        if (a.first > b.first) {
+            return true;
+        }
+    }
+    return false;
+}
+
 // For each document returns its id and relevance
 vector<pair<int, int>> FindAllDocuments(
         const map<string, set<int>>& word_to_documents,
@@ -89,20 +100,24 @@ vector<pair<int, int>> FindAllDocuments(
     for (auto [document_id, relevance] : document_to_relevance) {
         found_documents.push_back({document_id, relevance});
     }
+    sort(found_documents.begin(), found_documents.end()
+         , ComparePairBySecondThenFirst
+         );
+    
     return found_documents;
 }
 
-bool ComparePairBySecond(pair<int, int> a, pair<int, int> b) {
-    return a.second > b.second;
-}
 
-vector<pair<int, int>>  FindTopDocuments(
-         const map<string, set<int>>& word_to_documents,
-         const set<string>& stop_words,
-         const string& query) {
+
+vector<pair<int, int>>  FindTopDocuments( const map<string, set<int>>& word_to_documents, const set<string>& stop_words, const string& query) {
     vector<pair<int, int>> documents = FindAllDocuments(word_to_documents, stop_words, query);
-    sort(documents.begin(), documents.end(), ComparePairBySecond);
-    return documents;
+    // если размер меньше ограничения то возвращаю без изменений
+    if (documents.size() <= MAX_RESULT_DOCUMENT_COUNT) {
+        return documents;
+    }
+    // если размер больше то обрезаю
+    vector<pair<int, int>> topDocuments(documents.begin(), documents.begin()+MAX_RESULT_DOCUMENT_COUNT);
+    return topDocuments;
 }
 
 int main() {
@@ -118,7 +133,31 @@ int main() {
 
     const string query = ReadLine();
     // FindTopDocuments goes here
+    vector<pair<int, int>> top_documents = FindTopDocuments(word_to_documents, stop_words, query);
+    
     for (auto [document_id, relevance] : FindTopDocuments(word_to_documents, stop_words, query)) {
         cout << "{ document_id = "s << document_id << ", relevance = "s << relevance << " }"s << endl;
     }
 }
+
+// что делать если документы пустые
+
+/*
+ a the on cat
+ 6
+ a fat cat sat on a mat and ate a fat rat
+ fat rat
+ a fat cat rat
+ a fat cat sat
+ a fat cat
+ a fat dog
+ funny fat cat on a mat with rat
+ */
+
+/*
+ 
+ 2
+ hi bye
+ hi
+ hi buy
+ */
