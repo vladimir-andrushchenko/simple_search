5b6ff0ef482f77a5fbdd921116e6eb7a385ea8eb
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-03-06 22:12:30 +0300

    this seems to be working

--- simple_search/main.cpp
+++ simple_search/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -42,16 +43,15 @@ vector<string> SplitIntoWords(const string& text) {
         
         return words;
 }
-
 // ========================================
 
 struct Document {
     int id;
-    int relevance;
+    double relevance;
 };
 
 struct Query {
-    vstr words;
+    map<string, double> words_to_idf; // idf - indirect document frequence log(все_документы/документ_где_слово встречается) чем чаще встречается слово тем ниже его релевантность
     vstr minus_words;
 };
 
@@ -64,30 +64,35 @@ public:
     }
 
     void AddDocument(int document_id, const string& document) {
-            for (const string& word : SplitIntoWordsNoStop(document)) {
-                    word_to_documents_[word].insert(document_id);
-            }
+        vstr words_in_document = SplitIntoWordsNoStop(document);
+        for (const string& word : words_in_document) {
+            const double word_appears_n_times = count(words_in_document.begin(), words_in_document.end(), word);
+            const double tf = word_appears_n_times / words_in_document.size();
+            word_to_document_freqs_[word][document_id] = tf; // стоп слова при подсчете .size() не рассчитываются
+        }
+        ++number_of_documents_;
     }
 
     vector<Document> FindTopDocuments(const string& query) const {
-            auto matched_documents = FindAllDocuments(query);
-            
-            sort(
-                    matched_documents.begin(),
-                    matched_documents.end(),
-                    [](const Document& lhs, const Document& rhs) {
-                            return lhs.relevance > rhs.relevance;
-                    }
-            );
-            if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
-                matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
-            }
-            return matched_documents;
+        auto matched_documents = FindAllDocuments(query);
+        
+        sort(
+                matched_documents.begin(),
+                matched_documents.end(),
+                [](const Document& lhs, const Document& rhs) {
+                        return lhs.relevance > rhs.relevance;
+                }
+        );
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+        return matched_documents;
     }
 
 private:
-    map<string, set<int>> word_to_documents_;
+    map<string, map<int, double>> word_to_document_freqs_; // word -> map<document_id, term frequency in this document>
     set<string> stop_words_;
+    int number_of_documents_ = 0;
 
     vector<string> SplitIntoWordsNoStop(const string& text) const {
             vector<string> words;
@@ -103,16 +108,22 @@ private:
         Query query;
         for (const string& word : SplitIntoWords(query_raw)) {
             if (word[0] == '-'){
-                string word_without_minus_sign = word.substr(1);
-                if (stop_words_.count(word_without_minus_sign) == 1) {
+                const string stop_word_without_minus_sign = word.substr(1);
+                if (stop_words_.count(stop_word_without_minus_sign) == 1) {
                     continue;
                 }
-                query.minus_words.push_back(word_without_minus_sign);
+                query.minus_words.push_back(stop_word_without_minus_sign);
             } else {
                 if (stop_words_.count(word) == 1) {
                     continue;
                 }
-                query.words.push_back(word);
+                if (word_to_document_freqs_.count(word) == 1) { // смотрю если слово вообще встречается в каком либо документе
+                    const double in_how_many_docs_this_word_appears = word_to_document_freqs_.at(word).size();
+                    const double ratio_of_all_docs_to_this_word = number_of_documents_ / in_how_many_docs_this_word_appears;
+                    const double idf_of_word = log(ratio_of_all_docs_to_this_word);
+                    query.words_to_idf[word] = idf_of_word;
+                }
+                // если слово из запроса не встречается ни в одном документе оно не добавляется в структуру Query
             }
         }
         return query;
@@ -120,26 +131,29 @@ private:
     
 
     vector<Document> FindAllDocuments(const string& query_raw) const {
+        // сначала считаю релевантность
         const Query query = ParseQuery(query_raw);
-        map<int, int> document_to_relevance;
-        
-        for (const string& word : query.words) {
-            if (word_to_documents_.count(word) == 0) {
+        map<int, double> document_to_relevance;
+        for (const auto& [word, idf] : query.words_to_idf) { // цикл по словам запроса
+            if (word_to_document_freqs_.count(word) == 0) {
                 continue;
             }
-            for (const int document_id : word_to_documents_.at(word)) {
-                ++document_to_relevance[document_id];
+            for (const auto& [document_id, tf_of_word_in_this_doc]: word_to_document_freqs_.at(word)) { // цикл по документам где это слово встречается
+                document_to_relevance[document_id] += idf * tf_of_word_in_this_doc;
             }
         }
         
+        // потом удаляю документы в которых встречается минус слово
         for (const string& minus_word : query.minus_words) {
-            if (word_to_documents_.count(minus_word) == 0) {
+            if (word_to_document_freqs_.count(minus_word) == 0) {
                     continue;
             }
-            for (const int document_id : word_to_documents_.at(minus_word)) {
+            for (const auto& [document_id, _] : word_to_document_freqs_.at(minus_word)) {
                 document_to_relevance.erase(document_id);
             }
         }
+        
+        // потом всё вставляю в структурки и отправляю на выход
         vector<Document> matched_documents;
         for (auto [document_id, relevance] : document_to_relevance) {
             matched_documents.push_back({document_id, relevance});
@@ -170,3 +184,12 @@ int main() {
             cout << "{ document_id = " << document_id << ", relevance = " << relevance << " }" << endl;
     }
 }
+
+/*
+и в на
+3
+белый кот и модный ошейник
+пушистый кот пушистый хвост
+ухоженный пёс выразительные глаза
+пушистый ухоженный кот
+ */
