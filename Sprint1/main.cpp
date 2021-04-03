a2017bed8214dbed5d92634c88f1d97fcff407fb
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-04-03 16:30:57 +0300

    fixed indentation

--- Sprint1/main.cpp
+++ Sprint1/main.cpp
@@ -38,7 +38,7 @@ std::vector<std::string> SplitIntoWords(const std::string& text) {
 }
     
 struct Document {
-    int id = -1; // нумерация начинается с нуля. нетипичное значение для инициализации.
+    int id = 0;
     double relevance = 0;
     int rating = 0;
 };
@@ -62,7 +62,6 @@ public:
                      DocumentStatus status, const std::vector<int>& ratings) {
         const std::vector<std::string> words = SplitIntoWordsNoStop(document);
         
-        // inverse в смыслe (words.size() ^ -1) * words.size() == 1
         const double inverse_word_count = 1.0 / static_cast<double>(words.size());
         
         for (const std::string& word : words) {
@@ -87,34 +86,33 @@ public:
             const auto document_status = document_id_to_document_data_.at(document.id).status;
             const auto document_rating = document_id_to_document_data_.at(document.id).rating;
             
-            if(predicate(document.id, document_status, document_rating)) {
+            if (predicate(document.id, document_status, document_rating)) {
                 filtered_documents.push_back(document);
             }
         }
         
         std::sort(filtered_documents.begin(), filtered_documents.end(),
-             [](const Document& left, const Document& right) {
-                if (abs(left.relevance - right.relevance) < kMinSignificantDifferenceInRelevance) {
-                    return left.rating > right.rating;
-                } else {
-                    return left.relevance > right.relevance;
-                }
-             });
+                  [](const Document& left, const Document& right) {
+                      if (abs(left.relevance - right.relevance) < kAccuracy) {
+                          return left.rating > right.rating;
+                      } else {
+                          return left.relevance > right.relevance;
+                      }
+                  });
         
-        if (static_cast<int>(filtered_documents.size()) > kLimitForHowManyTopDocumentsToReturn) {
-            filtered_documents.resize(static_cast<size_t>(kLimitForHowManyTopDocumentsToReturn));
+        if (static_cast<int>(filtered_documents.size()) > kMaxResultDocumentCount) {
+            filtered_documents.resize(static_cast<size_t>(kMaxResultDocumentCount));
         }
         
         return filtered_documents;
     } // FindTopDocuments
     
     std::vector<Document> FindTopDocuments(const std::string& raw_query,
-                                      const DocumentStatus& desired_status = DocumentStatus::kActual) const {
-        const auto predicate = [desired_status]([[maybe_unused]] int document_id,
-                                               DocumentStatus document_status,
-                                               [[maybe_unused]] int rating) {
-                                                    return document_status == desired_status;
-                                                };
+                                           const DocumentStatus& desired_status = DocumentStatus::kActual) const {
+        const auto predicate = [desired_status]([[maybe_unused]] int document_id, DocumentStatus document_status,
+                                                [[maybe_unused]] int rating) {
+            return document_status == desired_status;
+        };
         
         return FindTopDocuments(raw_query, predicate);
     } // FindTopDocuments with status as a second argument
@@ -160,14 +158,14 @@ private:
     };
     
     struct QueryWord {
-        std::string data = "";
+        std::string data;
         bool is_minus = false;
         bool is_stop = false;
     };
     
 private:
     static constexpr int kMaxResultDocumentCount = 5;
-    static constexpr double kMinSignificantDifferenceInRelevance = 1e-6;
+    static constexpr double kAccuracy = 1e-6;
     
 private:
     std::vector<std::string> SplitIntoWordsNoStop(const std::string& text) const {
@@ -228,7 +226,7 @@ private:
     // Existence required
     double ComputeWordInverseDocumentFrequency(const std::string& word) const {
         const double number_of_documents_constains_word =
-                        static_cast<double>(word_to_document_id_to_term_frequency_.at(word).size());
+            static_cast<double>(word_to_document_id_to_term_frequency_.at(word).size());
         
         return log(static_cast<double>(GetDocumentCount()) / number_of_documents_constains_word);
     } // ComputeWordInverseDocumentFrequency
@@ -275,18 +273,15 @@ private:
     std::map<int, DocumentData> document_id_to_document_data_;
 };
 
-// ==================== для примера =========================
-
 void PrintDocument(const Document& document) {
     std::cout << "{ "
-         << "document_id = " << document.id << ", "
-         << "relevance = " << document.relevance << ", "
-         << "rating = " << document.rating
-         << " }" << std::endl;
+        << "document_id = " << document.id << ", "
+        << "relevance = " << document.relevance << ", "
+        << "rating = " << document.rating
+        << " }" << std::endl;
 }
 
 int main() {
-    // создание сервера и добавление документов
     SearchServer search_server;
     search_server.SetStopWords("и в на");
 
@@ -295,19 +290,15 @@ int main() {
     search_server.AddDocument(2, "ухоженный пёс выразительные глаза", DocumentStatus::kActual, {5, -12, 2, 1});
     search_server.AddDocument(3, "ухоженный скворец евгений",         DocumentStatus::kBanned, {9});
 
-    // поиск документов со статусом kActual
     std::cout << "ACTUAL by default:" << std::endl;
     for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот")) {
         PrintDocument(document);
     }
     
-    // поиск документов со статусом kActual
-    const auto find_documents_with_actual_status =
-                                     []([[maybe_unused]] int document_id,
-                                     DocumentStatus status,
-                                     [[maybe_unused]] int relevance) {
-                                         return status == DocumentStatus::kActual;
-                                     };
+    const auto find_documents_with_actual_status = []([[maybe_unused]] int document_id, DocumentStatus status,
+                                                      [[maybe_unused]] int relevance) {
+            return status == DocumentStatus::kActual;
+        };
                                     
     std::cout << "ACTUAL:" << std::endl;
     for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот",
@@ -315,20 +306,16 @@ int main() {
         PrintDocument(document);
     }
     
-    // поиск документов со статусом kBanned
     std::cout << "BANNED:" << std::endl;
         for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот",
                                                                        DocumentStatus::kBanned)) {
             PrintDocument(document);
         }
 
-    // поиск документов с четным id
-    const auto find_documents_with_even_id_numbers =
-                                    [](int document_id,
-                                    [[maybe_unused]] DocumentStatus status,
-                                    [[maybe_unused]] int relevance) {
-                                        return document_id % 2 == 0;
-                                    };
+    const auto find_documents_with_even_id_numbers = [](int document_id, [[maybe_unused]] DocumentStatus status,
+                                                        [[maybe_unused]] int relevance) {
+        return document_id % 2 == 0;
+    };
     
     std::cout << "Even ids:" << std::endl;
     for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот",
