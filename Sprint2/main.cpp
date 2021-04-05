58eabd7d3b45a1a32dfdd35c7cf3766fce5698a5
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-04-05 18:01:42 +0300

    initial commit

---
+++ Sprint2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sprint2
+//
+//  Created by Vladimir Andrushchenko on 03.04.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
