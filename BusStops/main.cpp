edef7014bd1775693a4798fde4f80b05eb8e787e
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-04-08 00:30:30 +0300

    tests fail

--- BusStops/main.cpp
+++ BusStops/main.cpp
@@ -41,14 +41,17 @@ istream& operator>>(istream& is, Query& q) {
         for (string& stop : q.stops) {
             is >> stop;
         }
+        
     } else if (operation_code == "BUSES_FOR_STOP"s) {
         q.type = QueryType::BusesForStop;
         
         is >> q.stop;
+        
     } else if (operation_code == "STOPS_FOR_BUS"s) {
         q.type = QueryType::StopsForBus;
         
         is >> q.bus;
+        
     } else if (operation_code == "ALL_BUSES"s) {
         q.type = QueryType::AllBuses;
     }
@@ -57,53 +60,97 @@ istream& operator>>(istream& is, Query& q) {
 }
 
 struct BusesForStopResponse {
-    // Наполните полями эту структуру
+    vector<string> buses;
 };
 
 ostream& operator<<(ostream& os, const BusesForStopResponse& r) {
-    // Реализуйте эту функцию
+    if (r.buses.empty()) {
+        os << "No stop"s << endl;
+    } else {
+        bool first = true;
+        for (const string& bus : r.buses) {
+            // avoid extra space at the end of the output
+            if (!first) {
+                os << ' ';
+            }
+            
+            os << bus;
+            
+            if (first) {
+                first = false;
+            }
+        }
+    }
     return os;
 }
 
 struct StopsForBusResponse {
-    // Наполните полями эту структуру
+    string bus;
+    map<string, vector<string>> stops_to_buses;
 };
 
 ostream& operator<<(ostream& os, const StopsForBusResponse& r) {
-    // Реализуйте эту функцию
+    if (r.stops_to_buses.empty()) {
+        cout << "No bus"s << endl;
+    } else {
+        for (const auto& [stop, buses] : r.stops_to_buses) {
+            cout << "Stop "s << stop << ": "s;
+            if (buses.size() == 1) {
+                cout << "no interchange"s;
+            } else {
+                for (const string& other_bus : buses) {
+                    if (r.bus != other_bus) {
+                        cout << other_bus << " "s;
+                    }
+                }
+            }
+            cout << endl;
+        }
+    }
+    
     return os;
 }
 
 struct AllBusesResponse {
-    // Наполните полями эту структуру
+    map<string, vector<string>> buses_to_stops;
 };
 
 ostream& operator<<(ostream& os, const AllBusesResponse& r) {
-    // Реализуйте эту функцию
+    if (r.buses_to_stops.empty()) {
+        cout << "No buses"s << endl;
+    } else {
+        for (const auto& bus_item : r.buses_to_stops) {
+            cout << "Bus "s << bus_item.first << ": "s;
+            for (const string& stop : bus_item.second) {
+                cout << stop << " "s;
+            }
+            cout << endl;
+        }
+    }
     return os;
 }
 
-class BusManager {
-public:
-    void AddBus(const string& bus, const vector<string>& stops) {
-        // Реализуйте этот метод
-    }
-
-    BusesForStopResponse GetBusesForStop(const string& stop) const {
-        // Реализуйте этот метод
-    }
-
-    StopsForBusResponse GetStopsForBus(const string& bus) const {
-        // Реализуйте этот метод
-    }
-
-    AllBusesResponse GetAllBuses() const {
-        // Реализуйте этот метод
-    }
-
-private:
-    map<string, vector<string>> buses_to_stops_, stops_to_buses_;
-};
+//class BusManager {
+//public:
+//    void AddBus(const string& bus, const vector<string>& stops) {
+//        // Реализуйте этот метод
+//    }
+//
+//    BusesForStopResponse GetBusesForStop(const string& stop) const {
+//        // Реализуйте этот метод
+//    }
+//
+//    StopsForBusResponse GetStopsForBus(const string& bus) const {
+//        // Реализуйте этот метод
+//    }
+//
+//    AllBusesResponse GetAllBuses() const {
+//        // Реализуйте этот метод
+//    }
+//
+//private:
+//    map<string, vector<string>> buses_to_stops_, stops_to_buses_;
+//};
 
 void TestQueryInputAllBuses() {
     istringstream input;
@@ -169,11 +216,62 @@ void TestQueryInputBusesForStop() {
     assert(query_buses_for_stop.stops.empty() == true);
 }
 
+void TestOutputBusesForStop() {
+    BusesForStopResponse response;
+    
+    response.buses = {"32"s, "32K"s};
+    
+    ostringstream output;
+    
+    output << response;
+    
+    assert(output.str() == "32 32K"s);
+}
+
+void TestOutputStopsForBus() {
+    StopsForBusResponse response;
+    
+    response.bus = "272"s;
+    response.stops_to_buses = {{"Vnukovo", {"272"s, "32"s, "32K"s, "950"s}},
+                               {"Moskovsky"s,{"272"s}},
+                               {"Rumyantsevo"s,{"272"s}},
+                               {"Troparyovo"s,{"272"s, "950"s}}
+                              };
+    
+    ostringstream output;
+    
+    cout << response;
+    
+    assert(output.str() == "Stop Vnukovo: 32 32K 950\n"s +
+                           "Stop Moskovsky: no interchange\n"s +
+                           "Stop Rumyantsevo: no interchange\n"s +
+                           "Stop Troparyovo: 950"s);
+}
+
+void TestOutputAllBuses() {
+    AllBusesResponse response;
+    
+    response.buses_to_stops = {{"272"s, {"Vnukovo"s, "Moskovsky"s, "Rumyantsevo"s, "Troparyovo"s}},
+                               {"32"s, {"Tolstopaltsevo"s, "Marushkino"s, "Vnukovo"s}},
+                              };
+    
+    ostringstream output;
+    
+    output << response;
+    
+    assert(output.str() == "Bus 272: Vnukovo Moskovsky Rumyantsevo Troparyovo\n"s +
+                           "Bus 32: Tolstopaltsevo Marushkino Vnukovo"s);
+}
+
 static void RunTests() {
     TestQueryInputNewBus();
     TestQueryInputAllBuses();
     TestQueryInputStopsForBus();
     TestQueryInputBusesForStop();
+    
+    TestOutputBusesForStop();
+//    TestOutputStopsForBus();
+    TestOutputAllBuses();
     cout << "all tests finished good" << endl;
 }
 
