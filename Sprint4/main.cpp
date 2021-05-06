7556c191d3f67acf26d7801d9a83c97b47fa10fd
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-05-06 12:22:17 +0300

    passes exercise

--- Sprint4/main.cpp
+++ Sprint4/main.cpp
@@ -365,66 +365,115 @@ private:
     std::vector<int> document_ids_;
 };
 
+template<typename InputIterator>
+class IteratorRange {
+public:
+    IteratorRange(InputIterator range_begin, InputIterator range_end): begin_iterator_(range_begin), end_iterator_(range_end) {}
+    
+public:
+    InputIterator begin() const {
+        return begin_iterator_;
+    }
+    
+    InputIterator end() const {
+        return end_iterator_;
+    }
+    
+    size_t size() const {
+        return std::distance(begin_iterator_, end_iterator_);
+    }
+    
+private:
+    InputIterator begin_iterator_;
+    InputIterator end_iterator_;
+};
+
+template <typename Iterator>
+class Paginator {
+public:
+    Paginator(Iterator range_begin, Iterator range_end, size_t page_size) {
+        if (range_begin == range_end) {
+            throw std::invalid_argument("no empty ranges"s);
+        }
+        
+        for (auto it = range_begin; it != range_end;) {
+            bool reached_end = false;
+            
+            for (size_t j = 1; j < page_size; ++j) {
+                if (it + j == range_end) {
+                    pages_.push_back({it, it + j});
+                    reached_end = true;
+                    break;
+                }
+            }
+            
+            if (reached_end) {
+                break;
+            }
+            
+            pages_.push_back({it, it + page_size});
+            it += page_size;
+        }
+    }
+    
+public:
+    auto begin() const {
+        return pages_.begin();
+    }
+    
+    auto end() const {
+        return pages_.end();
+    }
+    
+    size_t size() {
+        return pages_.size();
+    }
+    
+private:
+    std::vector<IteratorRange<Iterator>> pages_;
+};
+
+template <typename Container>
+auto Paginate(const Container& c, size_t page_size) {
+    return Paginator(std::begin(c), std::end(c), page_size);
+}
+
 void PrintDocument(const Document& document) {
     std::cout << "{ "s
     << "document_id = "s << document.id << ", "s
     << "relevance = "s << document.relevance << ", "s
-    << "rating = "s << document.rating << " }"s << std::endl;
+    << "rating = "s << document.rating << " }"s;
 }
 
-void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& words, DocumentStatus status) {
-    std::cout << "{ "s
-    << "document_id = "s << document_id << ", "s
-    << "status = "s << static_cast<int>(status) << ", "s
-    << "words ="s;
-    for (const std::string& word : words) {
-        std::cout << ' ' << word;
-    }
-    std::cout << "}"s << std::endl;
+std::ostream& operator<<(std::ostream& output, const Document& document) {
+    PrintDocument(document);
+    return output;
 }
 
-void AddDocument(SearchServer& search_server, int document_id, const std::string& document, DocumentStatus status,
-                 const std::vector<int>& ratings) {
-    try {
-        search_server.AddDocument(document_id, document, status, ratings);
-    } catch (const std::exception& e) {
-        std::cout << "Ошибка добавления документа "s << document_id << ": "s << e.what() << std::endl;
+template<typename IteratorRangeType>
+std::ostream& operator<<(std::ostream& output, const IteratorRange<IteratorRangeType>& iterator_range) {
+    for (auto it = iterator_range.begin(); it != iterator_range.end(); ++it) {
+        output << *it;
     }
-}
-
-void FindTopDocuments(const SearchServer& search_server, const std::string& raw_query) {
-    std::cout << "Результаты поиска по запросу: "s << raw_query << std::endl;
-    try {
-        for (const Document& document : search_server.FindTopDocuments(raw_query)) {
-            PrintDocument(document);
-        }
-    } catch (const std::exception& e) {
-        std::cout << "Ошибка поиска: "s << e.what() << std::endl;
-    }
-}
-
-void MatchDocuments(const SearchServer& search_server, const std::string& query) {
-    try {
-        std::cout << "Матчинг документов по запросу: "s << query << std::endl;
-        const int document_count = search_server.GetDocumentCount();
-        for (int index = 0; index < document_count; ++index) {
-            const int document_id = search_server.GetDocumentId(index);
-            const auto [words, status] = search_server.MatchDocument(query, document_id);
-            PrintMatchDocumentResult(document_id, words, status);
-        }
-    } catch (const std::exception& e) {
-        std::cout << "Ошибка матчинга документов на запрос "s << query << ": "s << e.what() << std::endl;
-    }
+    return output;
 }
 
 int main() {
-    SearchServer search_server;
-    
-    try {
-        search_server = SearchServer("и в на"s);
-    } catch (const std::invalid_argument& e) {
-        std::cout << "Ошибка создания search_server "s << ": "s << e.what() << std::endl;
+    SearchServer search_server("and with"s);
+
+    search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    search_server.AddDocument(3, "big cat nasty hair"s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "big dog cat Vladislav"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    search_server.AddDocument(5, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
+
+    const auto search_results = search_server.FindTopDocuments("curly dog"s);
+    int page_size = 2;
+    const auto pages = Paginate(search_results, page_size);
+
+    // Выводим найденные документы по страницам
+    for (auto page = pages.begin(); page != pages.end(); ++page) {
+        std::cout << *page << std::endl;
+        std::cout << "Page break"s << std::endl;
     }
-    
-    AddDocument(search_server, 1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
 }
