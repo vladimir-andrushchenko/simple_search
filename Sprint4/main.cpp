56326896d236bf7abf603168a5285cb50dd22625
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-05-13 13:14:59 +0300

    relocated helper functions

--- Sprint4/main.cpp
+++ Sprint4/main.cpp
@@ -7,64 +7,6 @@
 
 using namespace std::literals;
 
-void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& words, DocumentStatus status) {
-    std::cout << "{ "s
-    << "document_id = "s << document_id << ", "s
-    << "status = "s << static_cast<int>(status) << ", "s
-    << "words ="s;
-    for (const std::string& word : words) {
-        std::cout << ' ' << word;
-    }
-    std::cout << "}"s << std::endl;
-}
-
-void AddDocument(SearchServer& search_server, int document_id, const std::string& document, DocumentStatus status,
-                 const std::vector<int>& ratings) {
-    try {
-        search_server.AddDocument(document_id, document, status, ratings);
-    } catch (const std::exception& e) {
-        std::cout << "Ошибка добавления документа "s << document_id << ": "s << e.what() << std::endl;
-    }
-}
-
-void FindTopDocuments(const SearchServer& search_server, const std::string& raw_query) {
-    std::cout << "Результаты поиска по запросу: "s << raw_query << std::endl;
-    try {
-        for (const Document& document : search_server.FindTopDocuments(raw_query)) {
-            PrintDocument(document);
-        }
-        std::cout << std::endl;
-    } catch (const std::exception& e) {
-        std::cout << "Ошибка поиска: "s << e.what() << std::endl;
-    }
-}
-
-void MatchDocuments(const SearchServer& search_server, const std::string& query) {
-    try {
-        std::cout << "Матчинг документов по запросу: "s << query << std::endl;
-        const int document_count = search_server.GetDocumentCount();
-        for (int index = 0; index < document_count; ++index) {
-            const int document_id = search_server.GetDocumentId(index);
-            const auto [words, status] = search_server.MatchDocument(query, document_id);
-            PrintMatchDocumentResult(document_id, words, status);
-        }
-    } catch (const std::exception& e) {
-        std::cout << "Ошибка матчинга документов на запрос "s << query << ": "s << e.what() << std::endl;
-    }
-}
-
-SearchServer CreateSearchServer(const std::string& stop_words) {
-    SearchServer search_server;
-    
-    try {
-        search_server = SearchServer(stop_words);
-    } catch (const std::invalid_argument& e) {
-        std::cout << "Ошибка создания search_server "s << ": "s << e.what() << std::endl;
-    }
-    
-    return search_server;
-}
-
 int main() {
     TestSearchServer();
     std::cout << std::endl;
--- Sprint4/search_server.hpp
+++ Sprint4/search_server.hpp
@@ -130,4 +130,15 @@ std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_quer
     return filtered_documents;
 } // FindTopDocuments
 
+void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& words, DocumentStatus status);
+
+void AddDocument(SearchServer& search_server, int document_id, const std::string& document, DocumentStatus status,
+                 const std::vector<int>& ratings);
+
+void FindTopDocuments(const SearchServer& search_server, const std::string& raw_query);
+
+void MatchDocuments(const SearchServer& search_server, const std::string& query);
+
+SearchServer CreateSearchServer(const std::string& stop_words);
+
 #endif /* search_server_hpp */
