ad25cb50a4eab4e5a1a2cd828c2de43b3dd41632
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-05-18 21:16:03 +0300

    created Sprint5 folder

---
+++ Sprint5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sprint5
+//
+//  Created by Vladimir Andrushchenko on 18.05.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
