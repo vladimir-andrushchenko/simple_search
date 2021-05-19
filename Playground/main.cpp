5273bbd8bd368f206e371430a1d5addbf962f008
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-05-19 22:29:24 +0300

    doesn't work

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,81 +1,79 @@
-#include <chrono>
-#include <cstdlib>
-#include <iostream>
-#include <vector>
-
 #include "log_duration.h"
+
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-vector<int> ReverseVector(const vector<int>& source_vector) {
-    return {source_vector.rbegin(), source_vector.rend()};
-}
-
-int CountPops(const vector<int>& source_vector, int begin, int end) {
-    int res = 0;
-
-    for (int i = begin; i < end; ++i) {
-        if (source_vector[i]) {
-            ++res;
-        }
-    }
-
-    return res;
-}
-
-void FillRandom(vector<int>& v, int n) {
-    v.reserve(n);
+vector<float> ComputeAvgTemp(const vector<vector<float>>& vs) {
+    const auto measurements = vs[0].size();
     
-    for (int i = 0; i < n; i += 15) {
-        int number = rand();
-
-        // мы можем заполнить 15 элементов вектора,
-        // но не более, чем нам осталось до конца:
-        int count = min(15, n - i);
-
-        for (int j = 0; j < count; ++j)
-            // таким образом, получим j-й бит числа.
-            // операцию побитового сдвига вы уже видели в этой программе
-            // на этот раз двигаем вправо, чтобы нужный бит оказался самым последним
-            v.push_back((number >> j) % 2);
+    
+    vector<float> output(measurements, 0);
+    auto iterator = output.begin();
+    
+    for (const auto& day : vs) {
+        for (const float measurement : day) {
+            *iterator += (measurement > 0) ? measurement : 0;
+            ++iterator;
+        }
+        
+        iterator  = output.begin();
     }
+    
+    // нужно не учитывать отрицательные значения
+    for (float& measurements_sum : output) {
+        measurements_sum /= measurements;
+    }
+    
+    return output;
 }
 
-void Operate() {
-    LOG_DURATION("Total"s);
+//vector<float> GetRandomVector(int size) {
+//    static mt19937 engine;
+//    uniform_real_distribution<float> d(-100, 100);
+//
+//    vector<float> res(size);
+//    for (int i = 0; i < size; ++i) {
+//        res[i] = d(engine);
+//    }
+//
+//    return res;
+//}
 
-    vector<int> random_bits;
+void test() {
+    // 4 дня по 3 измерения
+    vector<vector<float>> v = {
+        {0, -1, -1},
+        {1, -2, -2},
+        {2, 3, -3},
+        {3, 4, -4}
+    };
 
-    // операция << для целых чисел это сдвиг всех бит в двоичной
-    // записи числа. Запишем с её помощью число 2 в степени 17 (131072)
-    static const int N = 1 << 17;
+    // среднее для 0-го измерения (1+2+3) / 3 = 2 (не учитывам 0)
+    // среднее для 1-го измерения (3+4) / 2 = 3.5 (не учитывам -1, -2)
+    // среднее для 2-го не определено (все температуры отрицательны), поэтому должен быть 0
 
-    // заполним вектор случайными числами 0 и 1
-    
-    {
-        LOG_DURATION("Fill random"s);
-        FillRandom(random_bits, N);
-    }
-
-    // перевернём вектор задом наперёд
-    vector<int> reversed_bits;
-    {
-        LOG_DURATION("Reverse"s);
-
-        reversed_bits = ReverseVector(random_bits);
-    }
-
-    {
-        LOG_DURATION("Counting"s);
-        // посчитаем процент единиц на начальных отрезках вектора
-        for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
-            double rate = CountPops(reversed_bits, 0, i) * 100. / i;
-            cout << "After "s << i << " bits we found "s << rate << "% pops"s << endl;
-        }
-    }
+    assert(ComputeAvgTemp(v) == vector<float>({2, 3.5f, 0}));
 }
 
 int main() {
-    Operate();
-    return 0;
+    test();
+//    vector<vector<float>> data;
+//    data.reserve(5000);
+//
+//    for (int i = 0; i < 5000; ++i) {
+//        data.push_back(GetRandomVector(5000));
+//    }
+//
+//    vector<float> avg;
+//    {
+//        LOG_DURATION("ComputeAvgTemp"s);
+//        avg = ComputeAvgTemp(data);
+//    }
+//
+//    cout << "Total mean: "s << accumulate(avg.begin(), avg.end(), 0.f) / avg.size() << endl;
 }
