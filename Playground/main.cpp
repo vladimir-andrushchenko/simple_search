69f7f022b728b4318e264d03e000e1b0f9228ce1
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-05-21 12:23:01 +0300

    passed lesson 10 sprint 5

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,40 +1,47 @@
+#include <algorithm>
+#include <cmath>
 #include <cstdint>
 #include <iostream>
+#include <random>
+#include <vector>
+
 
 using namespace std;
 
-// упростите эту экспоненциальную функцию,
-// реализовав линейный алгоритм
-int64_t T(int i) {
-    if (i <= 1) {
-        return 0;
-    }
-    if (i == 2) {
-        return 1;
-    }
-    
-    int first = 0, second = 0;
-    int third = 1;
-
-    for (int t = 3; t <= i; ++t) {
-        int curr = first + second + third;
-        first = second;
-        second = third;
-        third = curr;
+int EffectiveCount(const vector<int>& v, int n, int i) {
+    if ((static_cast<int64_t>(v.size()) * i / n) < log2(v.size())) {
+        cout << "Using find_if"s << endl;
+        
+        const auto it = find_if(v.begin(), v.end(), [i](int value){
+            return value > i;
+        });
+        
+        return static_cast<int>(distance(v.begin(), it));
     }
     
-    return third;
+    cout << "Using upper_bound"s << endl;
+    
+    const auto it = upper_bound(v.begin(), v.end(), i);
+    
+    return static_cast<int>(distance(v.begin(), it));
 }
 
 int main() {
-    int i;
+    static const int NUMBERS = 1'000'000;
+    static const int MAX = 1'000'000'000;
 
-    while (true) {
-        cout << "Enter index: "s;
-        if (!(cin >> i)) {
-            break;
-        }
+    mt19937 r;
+    uniform_int_distribution<int> uniform_dist(0, MAX);
 
-        cout << "Ti = "s << T(i) << endl;
+    vector<int> nums;
+    for (int i = 0; i < NUMBERS; ++i) {
+        int random_number = uniform_dist(r);
+        nums.push_back(random_number);
     }
+    sort(nums.begin(), nums.end());
+
+    int i;
+    cin >> i;
+    int result = EffectiveCount(nums, MAX, i);
+    cout << "Total numbers before "s << i << ": "s << result << endl;
 }
