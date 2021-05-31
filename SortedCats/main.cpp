63eadf5e9840ebda044bc1797a4e2c7075471abb
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-05-31 17:34:57 +0300

    pass

--- SortedCats/main.cpp
+++ SortedCats/main.cpp
@@ -64,13 +64,14 @@ ostream& operator<<(ostream& out, const Cat& cat) {
     return out;
 }
 
+
 // Возвращает массив указателей на элементы вектора cats, отсортированные с использованием
 // компаратора comp. Компаратор comp - функция, принимающая два аргумента типа const Cat&
 // и возвращающая true, если значения упорядочены, и false в ином случае
 template <typename Comparator>
 vector<const Cat*> GetSortedCats(const vector<Cat>& cats, const Comparator& comp) {
     vector<const Cat*> sorted_cat_pointers;
-    sorted_cat_pointers.reserve(cats.size());
+    //    sorted_cat_pointers.reserve(cats.size());
     
     for (const auto& cat : cats) {
         sorted_cat_pointers.push_back(&cat);
@@ -79,15 +80,16 @@ vector<const Cat*> GetSortedCats(const vector<Cat>& cats, const Comparator& comp
     std::sort(sorted_cat_pointers.begin(), sorted_cat_pointers.end(), [comp](const Cat* lhs, const Cat* rhs) {
         return comp(*lhs, *rhs);
     });
+    
     return sorted_cat_pointers;
 }
 
 // Выводит в поток out значения объектов, на который ссылаются указатели вектора cat_pointers.
 // Пример вывода элементов vector<const Cat*>:
 // {{Tom, male, breed: Bengal, age:2}, {Charlie, male, breed: Balinese, age:7}}
-// {{Tom, male, breed: Bengal, age:2}, {Charlie, male, breed: Balinese, age:7}}
+
 void PrintCatPointerValues(const vector<const Cat*>& cat_pointers, ostream& out) {
-    std::cout << '{';
+    out << "{"s;
     
     bool isFirst = true;
     
@@ -103,30 +105,35 @@ void PrintCatPointerValues(const vector<const Cat*>& cat_pointers, ostream& out)
         out << ", "s << *element;
     }
     
-    std::cout << '}';
+    out << "}"s;
 }
+
 
 int main() {
     const vector<Cat> cats = {
-        {"Tom"s, Gender::Male, CatBreed::Bengal, 2},
+        {"“Cat in Boots”"s, Gender::Male, CatBreed::Bengal, 2},
+        {"Leo"s, Gender::Male, CatBreed::Siberian, 3},
+        {"Luna"s, Gender::Female, CatBreed::Siamese, 1},
         {"Charlie"s, Gender::Male, CatBreed::Balinese, 7},
+        {"Ginger"s, Gender::Female, CatBreed::Sphynx, 5},
+        {"Tom"s, Gender::Male, CatBreed::Siamese, 2},
     };
-
+    
     {
         auto sorted_cats = GetSortedCats(cats, [](const Cat& lhs, const Cat& rhs) {
-            return tie(lhs.breed, lhs.name) < tie(rhs.breed, rhs.name);
+            return tie(lhs.age, lhs.name) < tie(rhs.age, rhs.name);
         });
-
-        cout << "Cats sorted by breed and name:"s << endl;
+        
+        cout << "Cats sorted by age and name:"s << endl;
         PrintCatPointerValues(sorted_cats, cout);
         cout << endl;
     }
-
+    
     {
         auto sorted_cats = GetSortedCats(cats, [](const Cat& lhs, const Cat& rhs) {
             return tie(lhs.gender, lhs.breed) < tie(rhs.gender, rhs.breed);
         });
-
+        
         cout << "Cats sorted by gender and breed:"s << endl;
         PrintCatPointerValues(sorted_cats, cout);
         cout << endl;
