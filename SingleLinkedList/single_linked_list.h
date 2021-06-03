6a7e2c27e5ea84ff9aed32aa070fedf580d59a65
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-06-03 12:54:06 +0300

    chore

---
+++ SingleLinkedList/single_linked_list.h
@@ -0,0 +1,57 @@
+#pragma once
+
+using namespace std;
+
+template <typename Type>
+class SingleLinkedList {
+public:
+    struct Node {
+        Node() = default;
+        
+        Node(const Type& val, Node* next)
+        : value(val)
+        , next_node(next) {
+        }
+        
+        Type value;
+        Node* next_node = nullptr;
+    };
+    
+public:
+    ~SingleLinkedList() {
+        Clear();
+    }
+    
+public:
+    // Возвращает количество элементов в списке за время O(1)
+    [[nodiscard]] size_t GetSize() const noexcept {
+        return size_;
+    }
+    
+    // Сообщает, пустой ли список за время O(1)
+    [[nodiscard]] bool IsEmpty() const noexcept {
+        return size_ == 0;
+    }
+    
+    // Вставляет элемент value в начало списка за время O(1)
+    void PushFront(const Type& value) {
+        head_.next_node = new Node(value, head_.next_node);
+        ++size_;
+    }
+    
+    // Очищает список за время O(N)
+    void Clear() noexcept {
+        while (head_.next_node) {
+            Node* current_node = head_.next_node;
+            head_.next_node = current_node->next_node;
+            delete current_node;
+            
+            --size_;
+        }
+    }
+    
+private:
+    Node head_{};
+    size_t size_ = 0;
+};
+
